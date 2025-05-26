7b587e2f5f4f425d7d2f16e5553d4f146dcc4b18
blackhawk-gazebos <blackhawks.nz@gmail.com>
2025-05-26 02:26:01 +0000

    list_omins_methods.php

--- list_omins_methods.php
+++ list_omins_methods.php
@@ -6,22 +6,26 @@ require_once 'jsonRPCClient.php';
 require_once '00_creds.php'; // defines $sys_id, $username, $password, $api_url
 
 $client = new jsonRPCClient($api_url, false);
-$creds  = (object)[ 'system_id'=>$sys_id, 'username'=>$username, 'password'=>$password ];
+$creds  = (object)[
+    'system_id' => $sys_id,
+    'username'  => $username,
+    'password'  => $password
+];
 
 try {
-    // Standard JSON-RPC introspection (may vary depending on server implementation)
-    // Try system.listMethods
-    \$methods = \$client->system_listMethods();
-} catch (Exception \$e) {
+    // Try the standard introspection call
+    $methods = $client->system_listMethods();
+} catch (Exception $e) {
     try {
-        // Fallback: listMethods without 'system'
-        \$methods = \$client->listMethods();
-    } catch (Exception \$e2) {
-        // Last resort: try rpc.listMethods
-        \$methods = \$client->rpc_listMethods();
+        // Fallback: without the "system_" prefix
+        $methods = $client->listMethods();
+    } catch (Exception $e2) {
+        // Last resort: RPC namespace
+        $methods = $client->rpc_listMethods();
     }
 }
 
-// Output result
 header('Content-Type: application/json');
-echo json_encode([ 'available_methods' => \$methods ], JSON_PRETTY_PRINT);
+echo json_encode([
+    'available_methods' => $methods
+], JSON_PRETTY_PRINT);
